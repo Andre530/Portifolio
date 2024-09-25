2691c4afa8f7d20d1c8f99e28172b1d2b326af2a
Andre530 <dallgnolfaustinoa@gmail.com>
2024-09-25 15:24:31 -0300

    ata
    desisti dessa porra, codigo filho da puta vou fazer outro fds

--- index.js
+++
@@ -1,7 +0,0 @@
-let header = document.getElementById("cabeçalho")
-let h4 = document.createElement('h4')
-h4.textContent = 'faustino.3910@aluno.pr.senac.br'
-h4.style.fontFamily = 'Cormorant Unicase'
-header.appendChild(h4)
-header.style.display = 'flex'
-header.style.flexDirection = 'row-reverse'
